3e5847926598098419bddb563a8f7932d2651ee8
medhani premathilaka <mpremathilaka1@gmail.com>
2025-11-11 10:35:35 +0530

    feat: add type checking step to CI workflow and update test case to reflect correct message

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloParagraph = screen.getByText(/hello fabriqfrontend/i);
+  expect(helloParagraph).toBeInTheDocument();
 });
