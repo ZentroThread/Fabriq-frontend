1705e4080c4650e40f94ce67afc09772b3257efd
medhani premathilaka <mpremathilaka1@gmail.com>
2025-11-19 16:01:10 +0530

    refactor : updated react

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hello message', () => {
-  render(<App />);
-  const helloParagraph = screen.getByText(/hello fabriqfrontend/i);
-  expect(helloParagraph).toBeInTheDocument();
-});
