a4b3d84fff6ed92e598d6d8d880860da89c1f10d
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2025-11-22 23:45:53 +0530

    feat: implement employee overview and profile pages with salary history functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import EmployeeOverview from "./pages/EmployeeOverview";
+import EmployeeProfile from "./pages/employee/EmployeeProfile";
+import { SalaryHistory } from "./pages/employee/SalaryHistory";
+
 
 function App() {
   return (
@@ -8,6 +12,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/emp" element={<SalaryHistory />} />
       </Routes>
     </Layout>
   );
