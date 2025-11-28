0e640c7716c3649e12982d0d1e5526b995f8314a
haanieahzan <haanieahzan@gmail.com>
2025-11-28 12:48:24 +0530

    login page updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import AppRoutes from "./routes/routes";
-import { ThemeProvider } from "./providers/theme-provider";
 
-function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AppRoutes />
-    </ThemeProvider>
-  );
-}
 
-export default App;
-import Layout from "./components/organisms/layout/layout";
 import Routes from "./routes/routes";
 import { ThemeProvider } from "./providers/theme-provider";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Layout>
+      
         <Routes />
-      </Layout>
+      
     </ThemeProvider>
   );
 }
--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import { useNavigate } from "react-router-dom";
 
 import Support from "../support/support";
 import SidebarButton from "../../atoms/iconbutton/side-button";
@@ -9,38 +8,6 @@ import type { UserRole } from "../../../config/sidebar-items";
 
 function Sidebar({ open: _open }: { open: boolean }) {
   const [activeRoute, setActiveRoute] = useState<string>("/");
-
-  //get the user role dynamically from JWT auth
-  const role = (localStorage.getItem("role") as UserRole) || "owner";
-
-  const items = sidebarItems[role];
-
-  return (
-    <div className="w-[300px] h-full m-0 shadow-lg flex flex-col bg-sidebar-bg">
-      {items.map((item) => (
-        <SidebarButton
-          key={item.to}
-          icon={item.icon}
-          label={item.label}
-          to={item.to}
-          active={activeRoute === item.to}
-          onClick={() => setActiveRoute(item.to)}
-        />
-      ))}
-      <Support />
-    </div>
-  );
-}
-
-export default Sidebar;
-import { useState } from "react";
-import Support from "../support/support";
-import SidebarButton from "../../atoms/iconbutton/side-button";
-import { sidebarItems } from "../../../config/sidebar-items";
-import type { UserRole } from "../../../config/sidebar-items";
-
-function Sidebar({ open: _open }: { open: boolean }) {
-  const [activeRoute, setActiveRoute] = useState<string>("/dashboard");
 
   //get the user role dynamically from JWT auth
   const role = (localStorage.getItem("role") as UserRole) || "owner";
