0e6f77e83412a36d1fee0058dafc01ae2e122b3e
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2025-12-14 01:10:48 +0530

    fix: Add eslint directive to prevent warning in updateFormData function

--- src/pages/employee-profile.tsx
+++ src/pages/employee-profile.tsx
@@ -22,6 +22,7 @@ export default function EmployeeProfile() {
 
   const updateFormData = useCallback(() => {
     if (employee) {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
       setFormData(employee);
     }
   }, [employee]);
